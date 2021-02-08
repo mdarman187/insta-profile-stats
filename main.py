5e0f8f8788eab43ae12212ea3e5b4ae5faaf3c09
MD ARMAN <muhammadarman187@gmail.com>
2021-02-08 21:47:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.instagram.com/{}/"
+URL = "https://www.instagram.com/{}/"
 
 def get_data(a):
+  data = {}
+  a = a.split("-")[0]
+  a = a.split(" ")
+  data['Followers'] = a[0]
+  data['Following'] = a[2]
+  data['Posts'] = a[4]
+  return data
+
+def scrape_data(username):
+  b = requests.get(URL.format(username))
+  a = BeautifulSoup(b.text, "html.parser")
+  meta = a.find("meta", property = "og:description")
+  return get_data(meta.attrs['content'])
+
+if __name__ == "__main__":
+  username = "mdarman_187"
+  data = scrape_data(username)
+  print("This account has ",data["Followers"],"followers")
+  print("This account has ",data["Following"],"following")
+  print("This account has ",data["Posts"],"posts")
+  
 
